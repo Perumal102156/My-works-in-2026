044c6ed7d1a048ad80ed5ce074a4c535da97f481
Perumal102156 <smvsk102156@gmail.com>
2026-08-21 12:20:15 +0530

    Update jenkins_build.py

--- Ticketing-System/jenkins_build.py
+++ Ticketing-System/jenkins_build.py
@@ -1,1 +1,9 @@
+print("====================================")
+print("       JENKINS PYTHON BUILD")
+print("====================================")
 
+print("Python is running inside Jenkins.")
+print("GitHub code was successfully downloaded.")
+print("Jenkins Python build completed.")
+
+print("====================================")
