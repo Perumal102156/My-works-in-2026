0b3043e123c381758c054db787b36029e86edd97
Venkat <smvsk102156@gmail.com>
2026-08-26 13:00:58 +0530

    Add automated

---
+++ Ticketing-System/test_app.py
@@ -0,0 +1,12 @@
+from app import app
+
+def test_homepage():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+if __name__ == "__main__":
+    test_homepage()
+    print("Application test PASSED")
